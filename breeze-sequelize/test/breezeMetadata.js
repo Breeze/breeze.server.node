7fde63211453fca5057dc566b5704152ce409428
Jay Traband <jayt@ideablade.com>
2014-09-02 20:38:43 -0700

    updated files

--- breeze-sequelize/test/breezeMetadata.js
+++ breeze-sequelize/test/breezeMetadata.js
@@ -1,16 +1,25 @@
 var breeze = require('breeze-client');
-var fs = require("fs");
-var should = require("should");
+var fs = require('fs');
+var should = require('should');
+// test should
+true.should.be.ok;
 
-describe("odata parse", function() {
-    this.enableTimeouts(false);
+describe('metadata', function () {
+  this.enableTimeouts(false);
 
+  it('should be able to import metadata', function() {
 
-    it("should be able to import metadata", function() {
-        var data = fs.readFileSync( './sampleMetadata.json', { encoding: 'utf8' } );
-        var ms = new breeze.MetadataStore();
-        ms.importMetadata(data);
-        ms.should
-    });
+    var data = fs.readFileSync('./test/sampleMetadata.json', { encoding: 'utf8' });
+    var ms = new breeze.MetadataStore();
+    ms.importMetadata(data);
+    var entityTypes = ms.getEntityTypes();
+    // entityTypes.should.be.instanceOf(Array).and.not.be.empty;
+
+    entityTypes.should.be.an.Array;
+    entityTypes.length.should.be.greaterThan(0);
+    // would rather do this but..
+    // don't know why but next line just hangs the test;
+    // entityTypes.should.not.be.empty;
+  });
 });
 
